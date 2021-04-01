c7f2061e286c75bccce681c7b5314361c86dd5c3
AnishaLamichhane <52813885+AnishaLamichhane@users.noreply.github.com>
2021-04-01 19:58:25 +0545

    Challenges.swift

--- project 2/project 2/ContentView.swift
+++ project 2/project 2/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     
     @State private var showingScore  = false
     @State private var scoreTitle = ""
+    @State private var score = 0 //Challenge 1
    
    @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
@@ -20,7 +21,8 @@ struct ContentView: View {
 //            Color.blue.edgesIgnoringSafeArea(.all)
             LinearGradient(gradient: Gradient(colors:[.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-            VStack(spacing: 30) {
+            VStack(spacing: 40) {
+                
                 VStack {
                     Text("Tap the flag of :")
                         .foregroundColor(.white)
@@ -48,20 +50,31 @@ struct ContentView: View {
                             .shadow(color: .black, radius: 2)
                     }
                 }
+                
+//                challenge 2
+                VStack (spacing: 150){
+                    Text("Total Score: \(score)")
+                        .foregroundColor(.white)
+                        .fontWeight(.black)
+                }
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
     }
+    
+//    challenge 1
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 5
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong!! that is the flag of \(countries[number])"  // challenge 3
+            score -= 3
         }
         showingScore = true
     }
@@ -77,4 +90,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
